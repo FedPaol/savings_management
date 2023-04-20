b2dcd223f7ddce6a17c49839e763fe84a860102d
FedPaol <fed.paolone@gmail.com>
2023-04-20 23:17:53 +0200

    first commit

---
+++ savings_management_BE/savings_management/src/main/java/com/savings_management/SavingsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.savings_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsManagementApplication.class, args);
+	}
+
+}
---
+++ savings_management_BE/savings_management/src/test/java/com/savings_management/SavingsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savings_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
